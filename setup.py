1f1f2c89ab74903abd7778dd6ed724bae107d4c4
Paul Durivage <pauldurivage@gmail.com>
2014-08-23 12:39:32 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='heartbeat',
+    version='1.0',
+    url='https://github.com/Storj/heartbeat',
+    license='The MIT License',
+    author='Storj Labs',
+    author_email='info@storj.io',
+    description='Python library for verifying existence of a file',
+    packages=['heartbeat'],
+)

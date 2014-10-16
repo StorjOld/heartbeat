c3391480564ab2191b5d90a6489816e8ca571ee0
Will James <jameswt@gmail.com>
2014-10-16 18:27:57 +1100

    PEP8 changes

--- heartbeat/Merkle/__init__.py
+++ heartbeat/Merkle/__init__.py
@@ -26,7 +26,7 @@
 # SOFTWARE.
 
 from .Merkle import Challenge, Tag, State, Proof, Merkle, MerkleHelper        # NOQA
-from .Merkle import DEFAULT_CHUNK_SIZE
+from .Merkle import DEFAULT_CHUNK_SIZE  # NOQA
 from .MerkleTree import MerkleTree     # NOQA
 
 __version__ = "0.1.4"
